861edb17dcb9a58dba653bf57435938bc81ad470
Augusto fragg <augustofragg@gmail.com>
2025-08-12 15:57:43 -0300

    Aula 08 - metodo get pt2

--- src/main/java/academy/devdojo/springboot2_essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2_essentials/controller/AnimeController.java
@@ -5,9 +5,9 @@ import academy.devdojo.springboot2_essentials.service.AnimeService;
 import academy.devdojo.springboot2_essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,9 +23,15 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+//        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 
 
--- src/main/java/academy/devdojo/springboot2_essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2_essentials/domain/Anime.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Anime {
-    private long id;
+    private Long id;
     private String name;
 
 
