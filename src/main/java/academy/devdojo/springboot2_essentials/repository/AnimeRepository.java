1145d3e9124b932daaf32336910b9c8b96b5df80
Augusto fragg <augustofragg@gmail.com>
2025-08-14 21:31:41 -0300

    Aula 16 - Request Params

--- src/main/java/academy/devdojo/springboot2_essentials/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2_essentials/repository/AnimeRepository.java
@@ -3,5 +3,8 @@ package academy.devdojo.springboot2_essentials.repository;
 import academy.devdojo.springboot2_essentials.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimeRepository extends JpaRepository<Anime,Long> {
+    List<Anime> findByName(String name);
 }
