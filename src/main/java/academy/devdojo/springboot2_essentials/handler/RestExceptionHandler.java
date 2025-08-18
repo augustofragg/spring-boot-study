92c370ce20904be51f066b10f6f58d36ae13c31b
Augusto fragg <augustofragg@gmail.com>
2025-08-18 16:09:48 -0300

    21Aula 21 Handler para validação de campos

--- src/main/java/academy/devdojo/springboot2_essentials/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2_essentials/handler/RestExceptionHandler.java
@@ -3,14 +3,21 @@ package academy.devdojo.springboot2_essentials.handler;
 
 import academy.devdojo.springboot2_essentials.Exception.BadRequestException;
 import academy.devdojo.springboot2_essentials.Exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2_essentials.Exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre) {
@@ -23,5 +30,27 @@ public class RestExceptionHandler {
                   .developerMessage(bre.getClass().getName() )
                   .build(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException
+            (MethodArgumentNotValidException exception) {
+        log.info("Fields {}",exception.getBindingResult().getFieldErrors());
+        List<FieldError> fieldErrors =  exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception. Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+
 
 }
--- src/main/java/academy/devdojo/springboot2_essentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2_essentials/requests/AnimePostRequestBody.java
@@ -1,12 +1,13 @@
 package academy.devdojo.springboot2_essentials.requests;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
    // @NotNull(message ="The anime name cannot be empty" )
     private String name;
+    private String url;
 }
